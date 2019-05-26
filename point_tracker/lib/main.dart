8e4f46848e2b93f7509c70624686c2ecf1c0ecdc
Kinnear <randomlaps@hotmail.com>
2019-05-26 18:40:11 +0800

    V1.0 MVP Working App
    * Implemented Expanded Widget for the score list for each person
    * Implemented UI for all of basic features in the app

--- point_tracker/lib/main.dart
+++ point_tracker/lib/main.dart
@@ -2,19 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'scoreModel.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  return runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    ScopedModel<ScoreModel>(
+    return ScopedModel<ScoreModel>(
         model: new ScoreModel(),
         child: MaterialApp(
           title: 'Points Tracker',
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
-          home: MyHomePage(title: 'Flutter Demo Home Page'),
+          home: MyHomePage(title: 'Points Tracker'),
         ));
   }
 }
@@ -28,19 +30,85 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   // Return the a row that describes the person's row
-  Widget buildRow() {
-    // return ScopedModel<ScoreModel>(model: )
-    return Row(
-      children: <Widget>[new Text("data")],
+  Widget buildPersonRow(int personIndex) {
+    return Container(
+      child:
+          ScopedModelDescendant<ScoreModel>(builder: (context, child, model) {
+        // This would contain the entire row with the person's name the list of scores and the final score
+        return Container(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+                margin: EdgeInsets.only(top: 2.0, bottom: 2.0),
+                padding : EdgeInsets.all(20),
+                width: 100,
+                color: Colors.blue[50],
+                child: Text(model.getPersonName(personIndex))),
+                Expanded(
+                  child:Container(
+                  margin: EdgeInsets.only(top: 2.0, bottom: 2.0),
+                width: 200,
+                color: Colors.blue[100],
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemCount: model
+                      .getPeopleScores()[personIndex]
+                      .getListOfScores()
+                      .length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 30.0,
+                      child: Center(
+                        child: Text(model
+                            .getPeopleScores()[personIndex]
+                            .getListOfScores()[index]
+                            .toString()),
+                      ),
+                    );
+                  },
+                )),),
+            Container(
+                color: Colors.blue[200],
+                margin: EdgeInsets.only(top: 2.0, bottom: 2.0),
+                padding : EdgeInsets.all(15.0),
+                width: 60.0,
+                child: Text(
+                    model
+                        .getPeopleScores()[personIndex]
+                        .getTotalScore()
+                        .toString(),
+                    style: TextStyle(fontSize: 17))),
+            Container(
+                width: 80.0,
+                child: FlatButton(
+                color: Colors.blue[300],
+                padding : EdgeInsets.all(15.0),
+                  child: Text('Add'),
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (_) {
+                          return AlertDialog(
+                            title: Text('Add Score Amount'), 
+                            content: TextField(
+                              keyboardType: TextInputType.numberWithOptions(),
+                              onSubmitted: (text) {
+                                model.addNewScoreToPersonByIndex(
+                                    int.parse(text), personIndex);
+                                Navigator.pop(context);
+                              },
+                            ),
+                          );
+                        });
+                    // launch pop up
+                  },
+                )),
+          ],
+        ));
+      }),
     );
   }
 
@@ -50,26 +118,50 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(child: buildRow()),
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
+      body: Container(
+          child: Align(
+              alignment: Alignment.topCenter,
+              child: ScopedModelDescendant<ScoreModel>(
+                builder: (context, child, model) {
+                  return Container(
+                    child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      // shrinkWrap: true,
+                      itemCount: model.getPeopleScores().length,
+                      itemBuilder: (context, index) {
+                        // size of the Row
+                        return Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 50,
+                            child: buildPersonRow(index));
+                      },
+                    ),
+                  );
+                },
+              ))),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          print("pressed");
+          showDialog(
+            context: context,
+            builder: (_) {
+              return AlertDialog(
+                title: Text('Add Person'),
+                content: TextField(
+                  keyboardType: TextInputType.text,
+                  onSubmitted: (text) {
+                    ScoreModel model = ScopedModel.of<ScoreModel>(context);
+                    model.addNewPerson(text);
+                    Navigator.pop(context);
+                  },
+                ),
+              );
+            },
+          );
+        },
+        tooltip: 'Add Person',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
--- point_tracker/lib/scoreModel.dart
+++ point_tracker/lib/scoreModel.dart
@@ -1,80 +1,104 @@
 import 'package:scoped_model/scoped_model.dart';
 
-class Person
-{ 
+class Person {
   String _name = "";
   List<int> _scores = new List<int>();
 
-  Person(String name)
-  {
+  Person(String name) {
     this._name = name;
   }
 
-  String getName()
-  {
+  String getName() {
     return _name;
   }
 
-  List<int> getListOfScores()
-  {
+  List<int> getListOfScores() {
     return _scores;
   }
 
-  void addNewScore(int newScore)
-  {
+  void addNewScore(int newScore) {
     _scores.add(newScore);
   }
 
-  int getTotalScore()
-  {
+  int getTotalScore() {
     int totalScore = 0;
-    for(int i = 0; i < _scores.length; i++)
-    {
+    for (int i = 0; i < _scores.length; i++) {
       totalScore += _scores[i];
     }
     return totalScore;
   }
 }
 
-class ScoreModel extends Model
-{
-    List<Person> _allPeople = new List<Person>();
+class ScoreModel extends Model {
+  ScoreModel() {
+    // addNewPerson('Kinnear');
+    // addNewScoreToPersonByName(10, 'Kinnear');
+    // addNewScoreToPersonByName(20, 'Kinnear');
+    // addNewScoreToPersonByName(20, 'Kinnear');
+    // addNewScoreToPersonByName(20, 'Kinnear');
+    // addNewScoreToPersonByName(20, 'Kinnear');
+    // addNewScoreToPersonByName(20, 'Kinnear');
+    // addNewScoreToPersonByName(20, 'Kinnear');
+    // addNewScoreToPersonByName(20, 'Kinnear');
+    // addNewScoreToPersonByName(20, 'Kinnear');
+    // addNewScoreToPersonByName(20, 'Kinnear');
+    // addNewScoreToPersonByName(20, 'Kinnear');
+    // addNewScoreToPersonByName(20, 'Kinnear');
+    // addNewScoreToPersonByName(20, 'Kinnear');
+    // addNewScoreToPersonByName(30, 'Kinnear');
 
-    // Adds a brand new person to our score list
-    bool addNewPerson(String name)
-    {
-      for(int i = 0; i < _allPeople.length; i++)
-      {
-        if(_allPeople[i].getName() == name)
-        {
-          return false;
-        }
+    // addNewPerson('Mika');
+    // addNewScoreToPersonByName(20, 'Mika');
+    // addNewScoreToPersonByName(20, 'Mika');
+    // addNewScoreToPersonByName(30, 'Mika');
+
+    // addNewPerson('Mika1');
+    // addNewScoreToPersonByName(20, 'Mika1');
+    // addNewScoreToPersonByName(20, 'Mika1');
+    // addNewScoreToPersonByName(30, 'Mika1');
+
+  }
+
+  List<Person> _allPeople = new List<Person>();
+  
+  String getPersonName(int index)
+  {
+    return _allPeople[index].getName();
+  }
+
+  // Adds a brand new person to our score list
+  bool addNewPerson(String name) {
+    for (int i = 0; i < _allPeople.length; i++) {
+      if (_allPeople[i].getName() == name) {
+        return false;
       }
+    }
 
-      _allPeople.add(new Person(name));
+    _allPeople.add(new Person(name));
+    notifyListeners();
+    return true;
+  }
+
+    // adds a new score to an existing person (via index identifier)
+  void addNewScoreToPersonByIndex(int score, int index) {
+      _allPeople[index].addNewScore(score);
       notifyListeners();
-      return true;
-    }
+  }
 
-    // adds a new score to an existing person
-    bool addNewScoreToPerson(int score, String name)
-    {
-      for(int i = 0; i < _allPeople.length; i++)
-      {
-        if(_allPeople[i].getName() == name)
-        {
-          _allPeople[i].addNewScore(score);
+  // adds a new score to an existing person (via string name identifier)
+  bool addNewScoreToPersonByName(int score, String name) {
+    for (int i = 0; i < _allPeople.length; i++) {
+      if (_allPeople[i].getName() == name) {
+        _allPeople[i].addNewScore(score);
 
-          notifyListeners();
-          return true;
-        }
+        notifyListeners();
+        return true;
       }
-      return false;
     }
+    return false;
+  }
 
-    List<Person> getPeopleScores()
-    {
-      return _allPeople;
-    }
-
+  List<Person> getPeopleScores() {
+    return _allPeople;
+  }
 }
